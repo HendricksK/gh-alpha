fd1d36548cb023e8df526b154cd5599f5d98532b
HendricksK <kurvinhendricks@gmail.com>
2020-06-05 22:28:29 +0200

    Updates

---
+++ precache-manifest.3011a2567a7bf276fc9be9a300806bec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a312b6f433b62839fe7a464017277b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40c5f4fcf9aea66c32fe",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "74187a4101cdc13fe91b",
+    "url": "/static/js/2.40b12be9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40b12be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c5f4fcf9aea66c32fe",
+    "url": "/static/js/main.8f2dd5af.chunk.js"
+  },
+  {
+    "revision": "ad0c433dfc3d9c334302",
+    "url": "/static/js/runtime-main.7fa36fd4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
